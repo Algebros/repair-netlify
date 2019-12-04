ce918476f9a03e440af21e6f20ac420966efaebf
Sofroniy <Sofroniy.m@gmail.com>
2019-12-04 19:11:36 +0300

    fix: eslint warnings and add tool picker

--- palette/assets/js/main.js
+++ palette/assets/js/main.js
@@ -15,6 +15,12 @@ const cols = canvas.width / pixelWidth;
 const rows = canvas.height / pixelHeight;
 const ctx = canvas.getContext('2d');
 
+const colorPicker = document.getElementById('colorPicker');
+const pencil = document.getElementById('pencil');
+const bucket = document.getElementById('bucket');
+const chooseColor = document.getElementById('chooseColor');
+const colorPalette = document.getElementById('colorPalette');
+
 function grid(color = 'white') {
   for (let i = 0; i < rows; i += 1) {
     const arrRow = [];
@@ -51,14 +57,15 @@ canvas.addEventListener('keyup', saveState);
 // ////////////////////////////////////////////////////////
 // ////////////////////////////////////////////////////////
 
+function addColor() {
+  draw(arr);
+  if (mouse.y < rows && mouse.x < cols) {
+    arr[mouse.y][mouse.x] = pixelColor;
+  }
+}
+
 function startDrawing(event) {
   if (event.button === 0) {
-    function addColor() {
-      draw(arr);
-      if (mouse.y < rows && mouse.x < cols) {
-        arr[mouse.y][mouse.x] = pixelColor;
-      }
-    }
     canvas.addEventListener('mousemove', addColor);
 
     canvas.addEventListener('mouseup', () => {
@@ -78,95 +85,10 @@ function fillBucket(event) {
   }
 }
 
-function palettePicker(e) {
-  const rect = canvas.getBoundingClientRect();
-  const x = e.pageX - rect.x;
-  const y = e.pageY - rect.y;
-  const c = this.getContext('2d');
-  const p = c.getImageData(x, y, 1, 1).data;
-  const hex = `#${(`000000${rgbToHex(p[0], p[1], p[2])}`).slice(-6)}`;
-  colorPicker.value = hex;
-  pixelColor = hex;
-
-  function rgbToHex(r, g, b) {
-    if (r > 255 || g > 255 || b > 255) throw 'Invalid color component';
-    return ((r << 16) | (g << 8) | b).toString(16);
-  }
-}
-
-// ////////////////////////////////////////////////////////
-// ////////////////////////////////////////////////////////
-
-pencil.addEventListener('click', () => {
-  canvas.removeEventListener('mousedown', fillBucket);
-  canvas.removeEventListener('mousedown', palettePicker);
-
-  canvas.addEventListener('mousedown', startDrawing);
-});
-
-document.addEventListener('keydown', (event) => {
-  if (event.keyCode === 80) {
-    canvas.removeEventListener('mousedown', fillBucket);
-    canvas.removeEventListener('mousedown', palettePicker);
-
-    canvas.addEventListener('mousedown', startDrawing);
-  }
-});
-
-// ////////////////////////////////////////////////////////
-// ////////////////////////////////////////////////////////
-
-bucket.addEventListener('click', () => {
-  canvas.removeEventListener('mousedown', startDrawing);
-  canvas.removeEventListener('mousedown', palettePicker);
-
-  canvas.addEventListener('mousedown', fillBucket);
-});
-
-document.addEventListener('keydown', (event) => {
-  if (event.keyCode === 66) {
-    canvas.removeEventListener('mousedown', startDrawing);
-    canvas.removeEventListener('mousedown', palettePicker);
-
-    canvas.addEventListener('mousedown', fillBucket);
-  }
-});
-
-// ////////////////////////////////////////////////////////
-// ////////////////////////////////////////////////////////
-
-chooseColor.addEventListener('click', () => {
-  canvas.removeEventListener('mousedown', startDrawing);
-  canvas.removeEventListener('mousedown', fillBucket);
-
-  canvas.addEventListener('mousedown', palettePicker);
-});
-
-document.addEventListener('keydown', (event) => {
-  if (event.keyCode === 67) {
-    canvas.removeEventListener('mousedown', startDrawing);
-    canvas.removeEventListener('mousedown', fillBucket);
-
-    canvas.addEventListener('mousedown', palettePicker);
-  }
-});
-
-// ////////////////////////////////////////////////////////
-// ////////////////////////////////////////////////////////
-
-function paletteColor(event) {
-  // console.log(getComputedStyle(event.target))
-  if (event.target.classList.contains('color')) {
-    // colorPalette.setAttribute('value', rgbStringToHex(event.target.style.backgroundColor));
-    colorPicker.value = rgbStringToHex(event.target.style.backgroundColor);
-    pixelColor = rgbStringToHex(event.target.style.backgroundColor);
-  }
-}
-
 function rgbStringToHex(rgb) {
-  const arr = rgb.split(/\D+/);
+  const newArr = rgb.split(/\D+/);
   let hex = '';
-  arr.forEach((element) => {
+  newArr.forEach((element) => {
     if (element !== '') {
       if (+element < 10) {
         hex += `0${element}`;
@@ -176,6 +98,96 @@ function rgbStringToHex(rgb) {
     }
   });
   return `#${hex}`;
+}
+
+function palettePicker(e) {
+  const rect = canvas.getBoundingClientRect();
+  const x = e.pageX - rect.x;
+  const y = e.pageY - rect.y;
+  const c = this.getContext('2d');
+  const p = c.getImageData(x, y, 1, 1).data;
+  const hex = rgbStringToHex(`(${p[0]}, ${p[1]}, ${p[2]})`);
+  colorPicker.value = hex;
+  pixelColor = hex;
+}
+
+// ////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////
+
+pencil.addEventListener('click', (event) => {
+  canvas.removeEventListener('mousedown', fillBucket);
+  canvas.removeEventListener('mousedown', palettePicker);
+  const active = document.querySelector('.active');
+  if (active !== null) active.classList.remove('active');
+  event.target.classList.add('active');
+  canvas.addEventListener('mousedown', startDrawing);
+});
+
+document.addEventListener('keydown', (event) => {
+  if (event.keyCode === 80) {
+    canvas.removeEventListener('mousedown', fillBucket);
+    canvas.removeEventListener('mousedown', palettePicker);
+    const active = document.querySelector('.active');
+    if (active !== null) active.classList.remove('active');
+    pencil.classList.add('active');
+    canvas.addEventListener('mousedown', startDrawing);
+  }
+});
+
+// ////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////
+
+bucket.addEventListener('click', (event) => {
+  canvas.removeEventListener('mousedown', startDrawing);
+  canvas.removeEventListener('mousedown', palettePicker);
+  const active = document.querySelector('.active');
+  if (active !== null) active.classList.remove('active');
+  event.target.classList.add('active');
+  canvas.addEventListener('mousedown', fillBucket);
+});
+
+document.addEventListener('keydown', (event) => {
+  if (event.keyCode === 66) {
+    canvas.removeEventListener('mousedown', startDrawing);
+    canvas.removeEventListener('mousedown', palettePicker);
+    const active = document.querySelector('.active');
+    if (active !== null) active.classList.remove('active');
+    bucket.classList.add('active');
+    canvas.addEventListener('mousedown', fillBucket);
+  }
+});
+
+// ////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////
+
+chooseColor.addEventListener('click', (event) => {
+  canvas.removeEventListener('mousedown', startDrawing);
+  canvas.removeEventListener('mousedown', fillBucket);
+  const active = document.querySelector('.active');
+  if (active !== null) active.classList.remove('active');
+  event.target.classList.add('active');
+  canvas.addEventListener('mousedown', palettePicker);
+});
+
+document.addEventListener('keydown', (event) => {
+  if (event.keyCode === 67) {
+    canvas.removeEventListener('mousedown', startDrawing);
+    canvas.removeEventListener('mousedown', fillBucket);
+    const active = document.querySelector('.active');
+    if (active !== null) active.classList.remove('active');
+    chooseColor.classList.add('active');
+    canvas.addEventListener('mousedown', palettePicker);
+  }
+});
+
+// ////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////
+
+function paletteColor(event) {
+  if (event.target.classList.contains('color')) {
+    colorPicker.value = rgbStringToHex(event.target.style.backgroundColor);
+    pixelColor = rgbStringToHex(event.target.style.backgroundColor);
+  }
 }
 
 colorPalette.addEventListener('click', paletteColor);
